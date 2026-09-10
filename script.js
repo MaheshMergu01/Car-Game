b1dca33f32666eddb1eb175b3f0cd1402fac548a
MaheshMergu01 <maheshmergu14@gmail.com>
2026-09-10 19:22:22 +0530

    Keydown Function

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("keydown",function(x){
+
+    if(x.keycode==87){
+        // alert("w")
+    }
+    if(x.keycode==65){
+        // alert("w")
+    }
+    if(x.keycode==83){
+        // alert("w")
+    }
+    if(x.keycode==68){
+        // alert("w")
+    }
+})
